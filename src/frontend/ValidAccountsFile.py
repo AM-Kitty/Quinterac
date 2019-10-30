08f03b70d159f0fad6843e886e8031262065a2c5
Jinting Zhang <47619156+JeanetteZhang@users.noreply.github.com>
2019-10-29 22:11:44 -0400

    Update ValidAccountsFile.py

--- src/frontend/ValidAccountsFile.py
+++ src/frontend/ValidAccountsFile.py
@@ -1,15 +1,18 @@
 import sys
+
 '''
 Class ValidAccountsFile:
 read valid accounts list file and write in new accounts
 '''
+
+
 class ValidAccountsFile:
 
     # read file from ValidAccountList file which contains a list of valid account number
     # convert it into a list form
     def readfile_ValidAccount(self):
         try:
-            file_data = open("ValidAccountListFile.txt", "r")
+            file_data = open("source_code/frontend/ValidAccountListFile.txt", "r")
             data = file_data.readlines()  # list type
             data = [int(i[:-1]) for i in data]
             return data[:-1]

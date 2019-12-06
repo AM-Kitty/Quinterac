a33ee0a9f5f390ab127edc17bc00c9247780f207
Kelly Ge <36249982+AM-Kitty@users.noreply.github.com>
2019-12-06 15:56:38 -0500

    update weekly and daily script

--- src/daily.py
+++ src/daily.py
@@ -3,9 +3,11 @@ import Backend
 import Frontend
 import os
 
-
+# create a “Daily” script to run fontend and backend
 def main():
+    # runs Front End over 3 transaction sessions
     number_of_transaction_sessions = 3
+    # save the output Transaction Summary File for each session in a separate file
     for i in range(number_of_transaction_sessions):
         sys.argv = [
             'Frontend.py',
@@ -13,23 +15,24 @@ def main():
             'DailySession/TransactionSummaryFile' + str(i+1) + '.txt']
         Frontend.main()
 
+    # concatenates the separate Transaction Summary Files into a Merged Transaction Summary file
     files = os.listdir("DailySession/")
-
     with open('TransactionSummaryFile.txt', 'w') as result:
         for f in files:
             for line in open("DailySession/" + f, 'r'):
                 result.write(line)
 
+    # remove separate Transaction Summary Files
     file = os.listdir("DailySession/")
     for i in file:
         os.remove("DailySession/" + i)
 
+    # runs your Back Office with the Merged Transaction Summary File as input
     sys.argv = [
         'Backend.py',
         'MasterAccountsFile.txt',
         'TransactionSummaryFile.txt']
     Backend.main()
-    #os.remove("TransactionSummaryFile.txt")
 
 
 if __name__ == "__main__":
--- src/weekly.py
+++ src/weekly.py
@@ -2,37 +2,37 @@ import sys
 import daily
 import io
 
-
+# create a “Weekly” script that runs the Daily script five separate times, simulating five days of operation
 def main():
     # Begin Weekly run with an empty Valid Accounts List and an empty Master Accounts File
     open("ValidAccountListFile.txt", 'w').close()
     open("MasterAccountsFile.txt", 'w').close()
-
+    # day one terminal input
     day_one = [
         'login', 'agent', 'createacct', 'Theo', '6666666', 'logout',
          'login', 'agent', 'createacct', 'Kelly', '8888888', 'logout',
          'login', 'agent', 'createacct', 'Sunny', '8888886', 'logout'
     ]
-
+    # day two terminal input
     day_two = [
         'login', 'agent', 'deposit', '6666666', '500', 'logout',
         'login', 'agent', 'deposit', '8888888', '600', 'logout',
         'login', 'agent', 'deposit', '8888886', '200', 'logout'
     ]
-
+    # day three terminal input
     day_three = [
         'login', 'atm', 'deposit', '6666666', '500', 'logout',
         'login', 'atm', 'deposit', '8888888', '600', 'logout',
         'login', 'atm', 'withdraw', '8888886', '200', 'logout'
     ]
-
+    # day four terminal input
     day_four = [
 
         'login', 'agent', 'deleteacct', 'Sunny', '8888886', 'logout',
         'login', 'agent', 'transfer', '8888888', '6666666', '600', 'logout',
         'login', 'agent', 'deposit', '6666666', '200', 'logout'
     ]
-
+    # day five terminal input
     day_five = [
         'login', 'agent', 'deposit', '6666666', '500', 'logout',
         'login', 'agent', 'deposit', '6666666', '600', 'logout',
@@ -42,7 +42,7 @@ def main():
     for i in days:
         sys.stdin = io.StringIO(
             '\n'.join(i))
-        # run the program
+        # run the daily program
         daily.main()
 
 
